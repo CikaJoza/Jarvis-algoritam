0cdd932189a283d052d249807b564764432143e6
Jovan Mosurović <75808461+CikaJoza@users.noreply.github.com>
2021-06-11 23:59:19 +0200

    Klasa Jarvis 1
    Kreiranje klase Jarvis, glavna klasa. Kreiranje metoda provere da nisu kolinearne tacke i metode za ispis

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.awt.*;
+
 public class Main {
 
     class Tacka {
@@ -7,7 +9,29 @@ public class Main {
         int y;
     }
 
+    public class Jarvis {
+
+        private boolean orijentacija(Point p, Point q, Point r) {
+            int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+
+            if (val >= 0)
+                return false;
+            return true;
+        }
+
+        public void ispisi(Point[] points, int[] next) {
+            System.out.println("\nTacke : ");
+            for (int i = 0; i < next.length; i++)
+                if (next[i] != -1)
+                    System.out.println("(" + points[i].x + ", " + points[i].y + ")");
+        }
+
+    }
+
+
+
+
     public static void main(String[] args) {
-       
+
     }
 }
